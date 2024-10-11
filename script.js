8e4e25499a78964d215da65ce7782c5512749c50
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-10-11 05:31:06 -0400

    feat: tasks can be added

---
+++ script.js
@@ -0,0 +1,6 @@
+function a() {
+    const li = document.createElement("li");
+    const textNode =document.createTextNode(document.getElementById("task").value);
+    li.appendChild(textNode);
+    document.getElementById("taskList").appendChild(li);
+}
