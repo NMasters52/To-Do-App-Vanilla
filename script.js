d006bce51490d541866897b6081d216763c49f2b
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-10-13 06:36:49 -0400

    adding local storage abilities. First step, create the ability to add task

--- script.js
+++ script.js
@@ -1,45 +1,58 @@
 
-function addTask() {
-    // create li to be added
-    const li = document.createElement("li");
+// function addTask() {
+//     // create li to be added
+//     const li = document.createElement("li");
 
-    // create text to be added to li
+//     // create text to be added to li
+//     const taskInput = document.getElementById("task");
+//     const taskText = taskInput.value;
+
+//     // create textNode to hold the text
+//     const textNode = document.createTextNode(taskText);
+//     li.appendChild(textNode);
+    
+
+//     // create button to be added with text
+//     const completeBtn = document.createElement("button");
+//     completeBtn.textContent = "complete task";
+
+//     // create clickevent for the button
+//     completeBtn.addEventListener("click", () => {
+//         li.style.textDecoration = "line-through";
+//     });
+
+//     // append completeBtn to the li
+//     li.appendChild(completeBtn);
+
+//     // create deleteBtn
+//     const deleteBtn = document.createElement("button");
+//     deleteBtn.textContent = "delete task";
+
+//     // deleteBtn onClick to delete element after being clicked
+//     deleteBtn.addEventListener("click", () => {
+//         li.remove();
+//     });
+
+//     // append delete btn to Li
+//     li.appendChild(deleteBtn);
+
+//     // add content to webpage
+//     document.getElementById("taskList").appendChild(li);
+    
+
+//     // delete text from input after submit
+//     taskInput.value = "";
+// }
+
+
+// setting up local storage
+
+// add task to local storage
+function addTask() {
     const taskInput = document.getElementById("task");
     const taskText = taskInput.value;
-
-    // create textNode to hold the text
-    const textNode = document.createTextNode(taskText);
-    li.appendChild(textNode);
-    
-
-    // create button to be added with text
-    const completeBtn = document.createElement("button");
-    completeBtn.textContent = "complete task";
-
-    // create clickevent for the button
-    completeBtn.addEventListener("click", () => {
-        li.style.textDecoration = "line-through";
-    });
-
-    // append completeBtn to the li
-    li.appendChild(completeBtn);
-
-    // create deleteBtn
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "delete task";
-
-    // deleteBtn onClick to delete element after being clicked
-    deleteBtn.addEventListener("click", () => {
-        li.remove();
-    });
-
-    // append delete btn to Li
-    li.appendChild(deleteBtn);
-
-    // add content to webpage
-    document.getElementById("taskList").appendChild(li);
-    
-
-    // delete text from input after submit
-    taskInput.value = "";
+    if (taskText) {
+        console.log("task added:", taskText);
+        taskInput.value = "";
+    }
 }
