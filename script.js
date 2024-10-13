b3eb57772493d8bb35340ec9e2b7d11b8c016bf9
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-10-13 05:38:03 -0400

    refractoring button code

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 function addTask() {
+    // create the li
     const li = document.createElement("li");
-    const textNode = document.createTextNode(document.getElementById
-    ("task").value);
-    const completeBtn = document.createElement("button");
 
+    // create text for li
+    const taskInput = document.getElementById("task");
+    const taskText = taskInput.value;
+
+    // create text node 
+    const textNode = document.createTextNode(taskText);
     li.appendChild(textNode);
+
+    // create the complete button
+    const completeBtn  = document.createElement("button");
+    
+
+    // add the task to the ul
+    li.appendChild(textNode);
+    // add button to newly added text
     li.appendChild(completeBtn);
     document.getElementById("taskList").appendChild(li);
 }
