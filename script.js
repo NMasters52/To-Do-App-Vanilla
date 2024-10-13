6208bae97690f1a69c817108e802e06f4d98cfa3
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-10-13 05:08:50 -0400

    add button to newly added task

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function addTask() {
     const li = document.createElement("li");
-    const textNode = document.createTextNode(document.getElementById("task").value);
+    const textNode = document.createTextNode(document.getElementById
+    ("task").value);
+    const completeBtn = document.createElement("button");
+
     li.appendChild(textNode);
+    li.appendChild(completeBtn);
     document.getElementById("taskList").appendChild(li);
 }
