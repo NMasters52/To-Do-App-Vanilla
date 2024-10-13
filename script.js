917b2c586c8582839921c329f4aa0fc33bed37aa
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-10-13 05:59:01 -0400

    feat: add text to task list. has button to complete task. text is removed from input after onClick

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
+
 function addTask() {
-    // create the li
+    // create li to be added
     const li = document.createElement("li");
 
-    // create text for li
+    // create text to be added to li
     const taskInput = document.getElementById("task");
     const taskText = taskInput.value;
 
-    // create text node 
+    // create textNode to hold the text
     const textNode = document.createTextNode(taskText);
     li.appendChild(textNode);
 
-    // create the complete button
-    const completeBtn  = document.createElement("button");
-    
+    // create button to be added with text
+    const completeBtn = document.createElement("button");
+    completeBtn.textContent = "complete";
 
-    // add the task to the ul
-    li.appendChild(textNode);
-    // add button to newly added text
+    // create clickevent for the button
+    completeBtn.addEventListener("click", () => {
+        li.style.textDecoration = "line-through";
+    })
+
+    // append button to the li
     li.appendChild(completeBtn);
+
+    // add content to webpage
     document.getElementById("taskList").appendChild(li);
+
+    taskInput.value = "";
 }
