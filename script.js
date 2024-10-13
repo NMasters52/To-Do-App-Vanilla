c20b42d4083bf288e7613e215725103e95c5769f
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-10-13 06:24:48 -0400

    first iteration without local storage. complete.

--- script.js
+++ script.js
@@ -10,21 +10,36 @@ function addTask() {
     // create textNode to hold the text
     const textNode = document.createTextNode(taskText);
     li.appendChild(textNode);
+    
 
     // create button to be added with text
     const completeBtn = document.createElement("button");
-    completeBtn.textContent = "complete";
+    completeBtn.textContent = "complete task";
 
     // create clickevent for the button
     completeBtn.addEventListener("click", () => {
         li.style.textDecoration = "line-through";
-    })
+    });
 
-    // append button to the li
+    // append completeBtn to the li
     li.appendChild(completeBtn);
+
+    // create deleteBtn
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete task";
+
+    // deleteBtn onClick to delete element after being clicked
+    deleteBtn.addEventListener("click", () => {
+        li.remove();
+    });
+
+    // append delete btn to Li
+    li.appendChild(deleteBtn);
 
     // add content to webpage
     document.getElementById("taskList").appendChild(li);
+    
 
+    // delete text from input after submit
     taskInput.value = "";
 }
