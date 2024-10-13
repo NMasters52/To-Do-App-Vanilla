1e02824478ddab5b97a54aad3c0c42b09725721d
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-10-13 05:06:13 -0400

    fix: change input onclick to a button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function addTask() {
     const li = document.createElement("li");
-    const textNode =document.createTextNode(document.getElementById("task").value);
+    const textNode = document.createTextNode(document.getElementById("task").value);
     li.appendChild(textNode);
     document.getElementById("taskList").appendChild(li);
 }
